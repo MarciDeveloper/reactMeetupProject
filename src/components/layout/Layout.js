46cec79f8a20798efad8b71c4804851b3ff7093b
Marcello Caccamo <londonliving89@gmail.com>
2021-12-18 11:57:45 +1000

    Final project upload

--- src/components/layout/Layout.js
+++ src/components/layout/Layout.js
@@ -1,14 +1,13 @@
-import MainNavigation from  './MainNavigation';
+import MainNavigation from './MainNavigation';
 import classes from './Layout.module.css';
 
-
-function Layout (props) {
-    return <div>
-        <MainNavigation />
-        <main className={classes.name}>
-            {props.children}
-        </main>
+function Layout(props) {
+  return (
+    <div>
+      <MainNavigation />
+      <main className={classes.main}>{props.children}</main>
     </div>
+  );
 }
 
 export default Layout;
--- src/components/meetups/MeetupList.js
+++ src/components/meetups/MeetupList.js
@@ -1,5 +1,5 @@
-import MeetupItem from "./MeetupItem";
-import classes from "./MeetupList.module.css";
+import MeetupItem from './MeetupItem';
+import classes from './MeetupList.module.css';
 
 function MeetupList(props) {
   return (
@@ -9,6 +9,7 @@ function MeetupList(props) {
           key={meetup.id}
           id={meetup.id}
           image={meetup.image}
+          title={meetup.title}
           address={meetup.address}
           description={meetup.description}
         />
